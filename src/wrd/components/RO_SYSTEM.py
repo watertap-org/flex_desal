b1f2d54504269b01c7daa4ce921f5ebfe48a4aae
Jake Churchill <jake.churchill@nrel.gov>
2025-11-19 19:14:32 -0500

    Outline of the RO + Pumps flowsheet

---
+++ src/wrd/components/RO_SYSTEM.py
@@ -0,0 +1,12 @@
+# Flowsheet that will include pumps and RO unit models for each stage
+
+# Feed
+# Feed Pump
+# Stage 1 (PRO1) 
+# Interstage Booster Pump 1
+# Stage 2 (PRO2)
+# Interstage Booster Pump 2
+# Stage Three (TRO)
+# Mixer for each stage's permeate
+# Permeate
+# Brine
