e4c78b4e1b50eed8fddf02793bfeb2620b85c498
kurbansitterley <kurban.sitterley@nrel.gov>
2025-12-02 18:33:39 -0700

    add initial RO test

---
+++ src/srp/tests/test_ro.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pyomo.environ import value, assert_optimal_termination, units as pyunits
+
+from watertap.core.solvers import get_solver
+
+import srp.components.ro as ro
+
+@pytest.mark.component
+def test_ro_main():
+    ro.main()
