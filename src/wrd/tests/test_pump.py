b58cee39f6613725dbac2d9a737f1c7ca02cab28
Jake Churchill <jake.churchill@nrel.gov>
2025-12-03 17:43:37 -0500

    Created pytest for the August 19, 2021 operating point

--- src/wrd/tests/test_pump.py
+++ src/wrd/tests/test_pump.py
@@ -1,8 +1,16 @@
 import pytest
+from pyomo.environ import units as pyunits
 
 from wrd.components.pump import main
 
 
 @pytest.mark.component
 def test_pump():
-    main()
+    # Conditions taken from ro_system_parameters_for_watertap.xlsx in Box August 19, 2021
+    Qin = 2637 / 264.2 / 60  # gpm to m3/s
+    Cin = 1055 * 0.5 / 1000  # us/cm to g/L
+    Pin = 35.4 / 14.5  # psi to bar
+    Pout = 141.9 / 14.5  # psi to bar
+    expected_power = 196.25  # kW
+    power = main(Qin=Qin, Cin=Cin, Pin=Pin, Pout=Pout)
+    assert power == pytest.approx(expected_power, rel=0.15)
