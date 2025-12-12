e81c0b30a16d5e4e730f7f11bd10394c1c742168
Jake Churchill <jake.churchill@nrel.gov>
2025-12-12 14:11:50 -0700

    Addition of UF component file as a generic separator

--- src/wrd/utilities.py
+++ src/wrd/utilities.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 from pyomo.environ import units as pyunits
-
+from idaes.models.unit_models import Mixer, Separator
 
 def load_config(config):
     with open(config, "r") as file:
@@ -80,3 +80,35 @@ def get_chem_list(yaml_name, section):
             chem_list.append(subsection)
         # chem_list.remove("default")
     return chem_list
+
+def touch_flow_and_conc(b):
+    """
+    Touch flow and conc variables for construction
+    """
+    props = b.find_component("properties")
+    if props is not None:
+        props[0].flow_vol_phase
+        props[0].conc_mass_phase_comp
+        return
+
+    cv = b.find_component("control_volume")
+    if cv is not None:
+        cv.properties_in[0].flow_vol_phase
+        cv.properties_in[0].conc_mass_phase_comp
+        cv.properties_out[0].flow_vol_phase
+        cv.properties_out[0].conc_mass_phase_comp
+        return
+
+    b.mixed_state[0].flow_vol_phase
+    b.mixed_state[0].conc_mass_phase_comp
+
+    if isinstance(b, Mixer):
+        for x in b.config.inlet_list:
+            sb = b.find_component(f"{x}_state")
+            sb[0].flow_vol_phase
+            sb[0].conc_mass_phase_comp
+    if isinstance(b, Separator):
+        for x in b.config.outlet_list:
+            sb = b.find_component(f"{x}_state")
+            sb[0].flow_vol_phase
+            sb[0].conc_mass_phase_comp
