7b356cc7246cd6605d0f4f3ff920de153db514cf
kurbansitterley <kurban.sitterley@nrel.gov>
2025-12-16 10:23:31 -0700

    test SEC

--- src/wrd/tests/test_pump.py
+++ src/wrd/tests/test_pump.py
@@ -1,10 +1,18 @@
 import pytest
-from pyomo.environ import units as pyunits
+from pyomo.environ import value, units as pyunits
 from wrd.components.pump import main
 
 
 @pytest.mark.component
 def test_pump_main():
+
+    # August 19, 2021 Data
+    # Stage 1 is default
     m = main()
+    assert pytest.approx(value(m.fs.costing.SEC), rel=1e-3) == 0.33924  # kWh/m3
+    # Stage 2
     m = main(Qin=1029, Pin=131.2 * pyunits.psi, stage_num=2)
-    m = main(Qin=384, Pin=112.6 * pyunits.psi, stage_num=3)
+    assert pytest.approx(value(m.fs.costing.SEC), rel=1e-3) == 0.07753
+    # Stage 3
+    m = main(Qin=384, Pin=(112.6 - 41.9) * pyunits.psi, stage_num=3)
+    assert pytest.approx(value(m.fs.costing.SEC), rel=1e-3) == 0.40512
