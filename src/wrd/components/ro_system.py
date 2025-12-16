bfd3fe31b789f04d5505705e9a2a7869cc63d665
kurbansitterley <kurban.sitterley@nrel.gov>
2025-12-15 17:44:29 -0700

    add repor ro system pumps func

--- src/wrd/components/ro_system.py
+++ src/wrd/components/ro_system.py
@@ -14,25 +14,21 @@ from idaes.core.util.initialization import propagate_state
 from idaes.core.util.model_statistics import degrees_of_freedom
 from idaes.core import FlowsheetBlock
 from idaes.models.unit_models import (
-    MixingType,
     MomentumMixingType,
     Mixer,
     Separator,
-    StateJunction,
     Feed,
     Product,
     SplittingType,
 )
-import idaes.core.util.scaling as iscale
 from idaes.core.util.scaling import calculate_scaling_factors
 
 from watertap.core.util.model_diagnostics.infeasible import *
 from watertap.property_models.NaCl_T_dep_prop_pack import NaClParameterBlock
-from watertap.unit_models.pressure_changer import Pump
 from watertap.core.solvers import get_solver
-from wrd.components.ro_train import *
 
-from wrd.utilities import load_config, get_config_value, get_config_file
+from wrd.components.ro_train import *
+from wrd.components.pump import report_pump
 from srp.utils import touch_flow_and_conc
 
 solver = get_solver()
@@ -54,7 +50,9 @@ def build_ro_system(m=None, num_trains=3, num_stages=3, prop_package=None):
     if prop_package is None:
         prop_package = m.fs.properties
 
-    m.fs.trains = Set(initialize=range(1, num_trains + 1))
+    m.num_trains = num_trains
+    m.num_stages = num_stages
+    m.fs.trains = Set(initialize=range(1, m.num_trains + 1))
     m.fs.train = FlowsheetBlock(m.fs.trains, dynamic=False)
 
     outlet_list = [f"train{i}" for i in m.fs.trains]
@@ -141,17 +139,16 @@ def build_ro_system(m=None, num_trains=3, num_stages=3, prop_package=None):
     return m
 
 
-def set_inlet_conditions(m, Qin=2637, Cin=0.5, file="wrd_ro_inputs_8_19_21.yaml"):
-
-    config_data = load_config(get_config_file(file))
-
-    Pout = get_config_value(config_data, "pump_outlet_pressure", "pumps", f"pump_1")
+def set_inlet_conditions(m, Qin=2637, Cin=0.5):
 
     m.fs.feed.properties.calculate_state(
         var_args={
-            ("flow_vol_phase", ("Liq")): Qin * pyunits.gallons / pyunits.minute,
+            ("flow_vol_phase", ("Liq")): m.num_trains
+            * Qin
+            * pyunits.gallons
+            / pyunits.minute,
             ("conc_mass_phase_comp", ("Liq", "NaCl")): Cin * pyunits.g / pyunits.L,
-            ("pressure", None): Pout,
+            ("pressure", None): 101325,
             ("temperature", None): 273.15 + 27,
         },
         hold_state=True,
@@ -228,14 +225,6 @@ def report_ro_system(m, w=30):
     print(f"\n\n{header}\n")
     print(f'{"Parameter":<{w}s}{"Value":<{w}s}{"Units":<{w}s}')
     print(f"{'-' * (3 * w)}")
-    # for i, inlet in enumerate(m.fs.product_mixer.config.inlet_list, 1):
-    #     sb = m.fs.product_mixer.find_component(f"{inlet}_state")
-    #     print(
-    #         f'{f"Train {i} Perm Conc":<{w}s}{value(pyunits.convert(sb[0].conc_mass_phase_comp["Liq", "NaCl"], to_units=pyunits.mg / pyunits.L)):<{w}.3f}{"mg/L"}'
-    #     )
-    #     print(
-    #         f'{f"Train {i} Perm Flow":<{w}s}{value(pyunits.convert(sb[0].flow_vol_phase["Liq"], to_units=pyunits.gallons / pyunits.minute)):<{w}.3f}{"gpm"}'
-    #     )
     print(
         f'{f"Total Perm Flow":<{w}s}{value(pyunits.convert(m.fs.product.properties[0].flow_vol_phase["Liq"], to_units=pyunits.gallons / pyunits.minute)):<{w}.3f}{"gpm"}'
     )
@@ -251,6 +240,18 @@ def report_ro_system(m, w=30):
     print(f'{f"Overall Recovery":<{w}s}{value(m.fs.recovery_vol)*100:<{w}.3f}{"%"}')
 
 
+def report_ro_system_pumps(m, w=30):
+
+    for i in m.fs.trains:
+        for j in m.fs.train[i].stages:
+            pump = m.fs.train[i].stage[j].pump
+            title = f"Train {i} Stage {j} Pump Report"
+            side = int(((3 * w) - len(title)) / 2) - 1
+            header = "*" * side + f" {title} " + "*" * side
+            print(f"\n{header}\n")
+            report_pump(pump, w=w)
+
+
 def main():
 
     m = build_ro_system(num_trains=4, num_stages=3)
@@ -263,7 +264,8 @@ def main():
     assert degrees_of_freedom(m) == 0
     results = solver.solve(m)
     assert_optimal_termination(results)
-    report_ro_system(m, w=40)
+    # report_ro_system(m, w=40)
+    report_ro_system_pumps(m, w=20)
 
     return m
 
