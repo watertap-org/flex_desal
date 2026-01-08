de12fa42c226562fcebe9b3cdc539593483d82d3
Jake Churchill <jake.churchill@nrel.gov>
2026-01-08 16:20:56 -0700

    Changing Permeability values and plotting one month

--- src/wrd/wrd_treatment_train.py
+++ src/wrd/wrd_treatment_train.py
@@ -775,6 +775,5 @@ def main(
 
 if __name__ == "__main__":
     num_pro_trains = 1
-    num_tsro_trains = 1
-    file = "wrd_inputs_3_13_21.yaml"
-    m = main(num_pro_trains=num_pro_trains, num_tsro_trains=num_tsro_trains, file=file)
+    file = "wrd_inputs_8_19_21.yaml"
+    m = main(num_pro_trains=num_pro_trains, file=file)
