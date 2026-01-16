5725633d63ed6031f64835c4f23f777a68b36451
Jake Churchill <jake.churchill@nrel.gov>
2026-01-16 12:10:16 -0700

    Added tests for brine and feed costs and cost tests for march

--- src/wrd/tests/test_wrd_treatment_train.py
+++ src/wrd/tests/test_wrd_treatment_train.py
@@ -10,12 +10,12 @@ def test_no_wrd_file():
     ):
         _ = main(num_pro_trains=2, file=None)
 
-
-@pytest.mark.parametrize("num_pro_trains", [1, 2, 3, 4])
+# BECAUSE TOTAL FLOWRATE NOW IN YAML, ONLY THE FULL 4 TRAINS CASE WILL WORK
+@pytest.mark.parametrize("num_pro_trains", [4])
 @pytest.mark.component
 def test_wrd_treatment_train_8_19_21(num_pro_trains):
     file = "wrd_inputs_8_19_21.yaml"
-    m = main(num_pro_trains=num_pro_trains, file=file)
+    m = main(num_pro_trains=num_pro_trains, uf_split_fraction=[0.4, 0.4, 0.2], file=file)
 
     if num_pro_trains == 4:
         # Expected chemical addition flows are from '2108 GRIP CHEMICALS REPORT.pdf'
@@ -57,12 +57,50 @@ def test_wrd_treatment_train_8_19_21(num_pro_trains):
         expected_LCOW = 0.442 * pyunits.USD_2021 / pyunits.m**3
         assert pytest.approx(value(m.fs.costing.LCOW), rel=0.35) == value(expected_LCOW)
 
-    # TODO: Add more tests against facility data
+     
+    # Brine
+    expected_brine_flow_cost = 59162 * pyunits.USD_2021 / pyunits.month
+    assert pytest.approx(value(m.fs.disposal.unit.costing.variable_operating_cost), rel=0.35) == value(expected_brine_flow_cost)
+    # Feedwater
+    expected_feed_flow_cost = 241400 * pyunits.USD_2021 / pyunits.month
+    assert pytest.approx(value(m.fs.feed.costing.variable_operating_cost), rel=0.35) == value(expected_feed_flow_cost)
+    # TODO: Add more tests against facility data. 
+    # power of some pumps, or total plant power use.
 
-
-@pytest.mark.parametrize("num_pro_trains", [1, 2, 3, 4])
+# BECAUSE TOTAL FLOWRATE NOW IN YAML, ONLY THE FULL 4 TRAINS CASE WILL WORK
+@pytest.mark.parametrize("num_pro_trains", [4])
 @pytest.mark.component
 def test_wrd_treatment_train_3_13_21(num_pro_trains):
     file = "wrd_inputs_3_13_21.yaml"
-    m = main(num_pro_trains=num_pro_trains, file=file)
-    # TODO: Add tests against facility data
+    m = main(num_pro_trains=num_pro_trains,num_tsro_trains=3, uf_split_fraction=[0.4, 0.4, 0.2], file=file)    
+    
+    if num_pro_trains == 4:
+        # Expected chemical addition flows are from '2108 GRIP CHEMICALS REPORT.pdf'
+        # March 13, 2021
+      
+        agg_cost_dict = {
+                "sodium_hypochlorite": 9488 * pyunits.USD_2021 / pyunits.month,
+                "calcium_hydroxide": 11153 * pyunits.USD_2021 / pyunits.month,
+                "sodium_bisulfite": 13835 * pyunits.USD_2021 / pyunits.month,
+                "scale_inhibitor": 13568 * pyunits.USD_2021 / pyunits.month,
+                "sulfuric_acid": 14376 * pyunits.USD_2021 / pyunits.month,
+                "sodium_hydroxide": 29233 * pyunits.USD_2021 / pyunits.month,
+                "ammonium_sulfate":  4997 * pyunits.USD_2021 / pyunits.month,
+                "electricity": 132498 * pyunits.USD_2021 / pyunits.month,
+            }
+        
+        for k, v in m.fs.costing.aggregate_flow_costs.items():
+            agg_cost = pyunits.convert(v, to_units=pyunits.USD_2021 / pyunits.month)
+            # Expected deviations due to daily variations in chemical use
+            # i.e., comparing a modeled daily cost to an actual monthly cost
+            assert pytest.approx(value(agg_cost), rel=0.35) == value(agg_cost_dict[k])
+
+        expected_LCOW = 0.395 * pyunits.USD_2021 / pyunits.m**3
+        assert pytest.approx(value(m.fs.costing.LCOW), rel=0.35) == value(expected_LCOW)
+    
+        # Brine
+        expected_brine_flow_cost = 61362 * pyunits.USD_2021 / pyunits.month
+        assert pytest.approx(value(m.fs.disposal.unit.costing.variable_operating_cost), rel=0.35) == value(expected_brine_flow_cost)
+        # Feedwater
+        expected_feed_flow_cost = 205820 * pyunits.USD_2021 / pyunits.month
+        assert pytest.approx(value(m.fs.feed.costing.variable_operating_cost), rel=0.35) == value(expected_feed_flow_cost)
