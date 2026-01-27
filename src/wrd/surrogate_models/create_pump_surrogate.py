2747ed78bb4156b250111a216d6e6c0e688e8054
Jake Churchill <jake.churchill@nrel.gov>
2026-01-27 15:01:54 -0700

    Added a plot and max flow point from head curves at different speeds

--- src/wrd/surrogate_models/create_pump_surrogate.py
+++ src/wrd/surrogate_models/create_pump_surrogate.py
@@ -3,12 +3,8 @@ import numpy as np
 import os
 
 from pyomo.environ import (
-    Param,
     Var,
-    Suffix,
-    NonNegativeReals,
     value,
-    units as pyunits,
     ConcreteModel,
 )
 
@@ -17,14 +13,7 @@ from idaes.core.surrogate.pysmo_surrogate import (
     PysmoPolyTrainer,
     PysmoSurrogate,
 )
-from idaes.core.surrogate.sampling.data_utils import (
-    split_training_validation,
-)  # Yes, it's a random split
-from idaes.core.surrogate.plotting.sm_plotter import (
-    surrogate_scatter2D,
-    surrogate_parity,
-    surrogate_residual,
-)
+
 from idaes.core.util.scaling import calculate_variable_from_constraint
 
 from idaes.core.surrogate.surrogate_block import SurrogateBlock
@@ -73,7 +62,7 @@ def head_limit(flow,pump_type):
 def MCSF(flow,pump_type):
     """Helper function to define max head for given flowrate along the minimum continuous stable flow."""
     # Coefficients from pump curve data
-    # Flow must be in units of scaled gpm. Head is in ft.
+    # Flow must be in units of scaled gpm. Head is in scaled ft.
     if pump_type == 'RO_feed':
         c_0 = 0.838
         c_1 = -1.869681
@@ -90,9 +79,25 @@ def MCSF(flow,pump_type):
     return head
 
 
+def max_flows(flow,pump_type):
+    """Helper function to define maximum flow for given flowrate along the maximum pump capacity across different speeds."""
+    # Flow must be in units of scaled gpm. Head is in scaled ft.
+    if pump_type == 'RO_feed':
+        d_0 = 0.19338
+        d_1 = -0.21424
+        d_2 = 0.23183
+        d_3 = -0.00942
+    if pump_type == 'RO_IS':
+        d_0 = 1600
+        d_1 = 0
+        d_2 = 0
+        d_3 = 0
+    head = d_0 + d_1 * (flow) + d_2 * (flow) ** 2 + d_3 * (flow) ** 3
+    return head
+
 
 # Select Types
-pump_type = 'RO_IS'
+pump_type = 'RO_feed'
 fittype = "rbf"
 
 # load data
@@ -166,7 +171,7 @@ m.surrogate_blk.pysmo_constraint.display()  # display()
 # minx, maxx = m.flowrate.bounds
 # miny, maxy = m.power.bounds
 
-num_points = 25
+num_points = 30
 x_vals = np.linspace(min_flow, max_flow, num=num_points)
 y_vals = np.linspace(min_head, max_head, num=num_points)
 z_vals = np.zeros((num_points, num_points))
@@ -178,6 +183,7 @@ for i in range(num_points):
             y_vals[j] > head_limit(x_vals[i],pump_type) 
             or y_vals[j] > MCSF(x_vals[i],pump_type)
             or y_vals[j] < min_head_limit(x_vals[i],pump_type)
+            or y_vals[j] < max_flows(x_vals[i],pump_type)
         ): 
             z_vals[i, j] = np.nan
             continue
@@ -219,59 +225,60 @@ elif pump_type == 'RO_IS':
     levels=[52,62,71,77,81,82]
     labels=["52%","62%","71%","77%","81%","82%"]
 
-
-iso_eff = plt.contour(
-    X,
-    Y,
-    Z,
-    levels=levels,
-    labels=labels,
-    colors='red',
-)
-plt.clabel(iso_eff, inline=True, fontsize=8, fmt="%1.0f%%", colors='black')
-
 plt.xlabel("Flow (gpm)")
 plt.ylabel("Head (ft)")
 plt.title(f"{pump_type.replace('_', ' ').title()} Pump Efficiency Contour Plot")
 
+# Data points vs. surrogate outputs
+X_data = pump_data["Flow (gpm)"] * 1e3
+Y_data = pump_data["Head (ft)"] * 1e2
+Z_data = pump_data["efficiency"] * 1e2
 
+for l in levels:
+    X_points = X_data[Z_data == l]
+    Y_points = Y_data[Z_data == l]    
+    plt.scatter(X_points, Y_points, color="red", s=10, label=f"{l}%")
+    plt.text(X_points.iloc[0]+20,Y_points.iloc[0]+3, f"{l}%",color="red")
 plt.show()
 
+# Data Validation Scatter plot
+
+Z_modeled = pd.DataFrame()
+
+for i in range(len(X_data)):
+    m.flowrate.fix(X_data[i]/1e3)
+    m.head.fix(Y_data[i]/1e2)
+    calculate_variable_from_constraint(m.eff, m.surrogate_blk.pysmo_constraint["efficiency"])
+    Z_modeled.at[i, "efficiency_modeled"] = value(m.eff)*1e2
 
 # # Create 3D scatter plot
-# fig2 = plt.figure(figsize=(10, 8))
-# ax2 = fig2.add_subplot(111, projection="3d")
+fig2 = plt.figure(figsize=(10, 8))
+ax2 = fig2.add_subplot(111, projection="3d")
 
-# # Flatten the data for scatter plot
+# Flatten the data for scatter plot
 # X_flat = X.flatten()
 # Y_flat = Y.flatten()
 # Z_flat = Z.flatten()
 
-# # Remove NaN values for cleaner plot
+# Remove NaN values for cleaner plot
 # mask = ~np.isnan(Z_flat)
 # X_clean = X_flat[mask]
 # Y_clean = Y_flat[mask]
 # Z_clean = Z_flat[mask]
 
-# # Create scatter plot with color mapping
-# scatter = ax2.scatter(
-#     X_clean, Y_clean, Z_clean, c=Z_clean, cmap="viridis", s=50, alpha=0.6
-# )
-# fig2.colorbar(scatter, ax=ax, label="Efficiency (%)")
+# Create scatter plot with color mapping
+scatter = ax2.scatter(X_data, Y_data, Z_data, color='red', s=50, alpha=0.6)
 
-# # Plot actual data points
-# X = pump_data["Flow (gpm)"] * 1e3
-# Y = pump_data["Head (ft)"] * 1e2
-# Z = pump_data["efficiency"] * 1e2
-# ax2.scatter(X, Y, Z, color="red", s=20, label="Data Points")
+# Plot actual data points
+ax2.scatter(X_data,Y_data,Z_modeled["efficiency_modeled"], color="black", s=20, label="Modeled Points")
 
-# ax2.set_xlabel("Flow (gpm)")
-# ax2.set_ylabel("Head (ft)")
-# ax2.set_zlabel("Efficiency (%)")
-# ax2.set_title("RO Feed Pump Efficiency 3D Scatter Plot")
-# ax2.set_xlim(0, 4500)
-# ax2.set_ylim(0, 400)
-# plt.show()
+ax2.set_xlabel("Flow (gpm)")
+ax2.set_ylabel("Head (ft)")
+ax2.set_zlabel("Efficiency (%)")
+ax2.set_title("RO Feed Pump Efficiency 3D Scatter Plot")
+ax2.set_xlim(0, 4500)
+ax2.set_ylim(0, 400)
+plt.show()
 
 
 # # Get the absolute path of the current script
