7902696ee802e9ff1b11804e18dce6894d2c14c1
Jake Churchill <jake.churchill@nrel.gov>
2026-01-28 11:21:47 -0700

    Creating dataset to build a surrogate on for Ro feed pump

--- src/wrd/tests/temp_test_pumps.py
+++ src/wrd/tests/temp_test_pumps.py
@@ -1,19 +1,40 @@
 import pytest
+import pandas as pd
+import numpy as np
 from pyomo.environ import value, units as pyunits
 from wrd.components.pump import main
 
+# TODO: Consider applying the param sweep tool that already exists in WaterTAP if using this to generate data for a surrogate model.
 
-def test_pump_param_sweep():
+def create_test_pairs(flow_ub = 2700,flow_lb = 2600,head_ub = 270,head_lb = 250,num_points = 2):
+
+    test_pairs = pd.DataFrame(columns=['flow','head'])
+    flow_vals = np.linspace(flow_lb, flow_ub, num=num_points)
+    head_vals = np.linspace(head_lb, head_ub, num=num_points)
+    test_pairs['flow'] = np.repeat(flow_vals, num_points)
+    test_pairs['head'] = np.tile(head_vals, num_points) 
+    return test_pairs
+
+def test_pump_param_sweep(test_pairs=None, pump_type='RO_feed', Pin=14.5):
     # RO Feed Pump
     # Flow and Head pairs
-    test_pairs = [(2675,276.4)]
-    for flow, head in test_pairs:
+    if test_pairs is None:
+        test_pairs = pd.DataFrame([[2778.0,271.1],[2778.0,150.8]], columns=['flow','head'])
+    outputs = pd.DataFrame(columns=['efficiency','speed'])
+    if pump_type == 'RO_feed':
+        stage_num = 1
+    elif pump_type == 'RO_IS':
+        stage_num = 2
+
+    for i in range(len(test_pairs)):
+        flow = test_pairs.iloc[i, 0]
+        head = test_pairs.iloc[i, 1]
         m = main(Qin= flow,
             head = head,
             Cin=0.5,
-            Tin=302,
-            Pin=14.5, #psi
-            stage_num=1,
+            Tin=298.15,
+            Pin=Pin, #psi
+            stage_num=stage_num,
             uf=False,
             file="wrd_inputs_8_19_21.yaml",
             add_costing=True,
@@ -24,8 +45,68 @@ def test_pump_param_sweep():
         print(f"Pump RPM: {rpm}")
         fluid_efficiency = value(m.fs.pump.unit.eff.efficiency_fluid)
         print(f"Fluid efficiency: {fluid_efficiency}")
+        outputs.at[i, 'efficiency'] = fluid_efficiency * 100
+        outputs.at[i, 'speed'] = speed * 100
+    test_pairs.reset_index(drop=True, inplace=True)
+    dataset = pd.concat([test_pairs, outputs], axis=1)
+    return dataset
 
+def head_limit(flow,pump_type):
+    """Helper function to define minimum head for given flowrate."""
+    # Coefficients from pump curve data
+    # Flow must be units of scaled gpm. Head is in ft.
+    if pump_type == 'RO_feed':
+        b_0 = 3.127555
+        b_1 = -0.09634
+        b_2 = 0.051555
+        b_3 = -0.026339
+    if pump_type == 'RO_IS':
+        b_0 = 1.02801
+        b_1 = -0.21038
+        b_2 = 0.284634
+        b_3 = -0.253384
+    head = (b_0 + b_1 * (flow) + b_2 * (flow)**2 + b_3 * (flow)**3)
+    return head
+
+def max_flows(flow,pump_type):
+    """Helper function to define maximum flow for given flowrate along the maximum pump capacity across different speeds."""
+    # Flow must be in units of scaled gpm. Head is in scaled ft.
+    if pump_type == 'RO_feed':
+        d_0 = 0.19338
+        d_1 = -0.21424
+        d_2 = 0.23183
+        d_3 = -0.00942
+    if pump_type == 'RO_IS':
+        # Missing!!!
+        d_0 = 1600
+        d_1 = 0
+        d_2 = 0
+        d_3 = 0
+    head = d_0 + d_1 * (flow) + d_2 * (flow) ** 2 + d_3 * (flow) ** 3
+    return head
+
+def filter_pump_test_points(pump_data, pump_type='RO_feed'):
+    # Filter out points above upper bound of pump curve
+    pump_data = pump_data[pump_data['head'] <= head_limit(pump_data['flow']/1e3, pump_type=pump_type)*1e2]
+    # Filter out points to the right of max flow at diff. speeds
+    pump_data = pump_data[pump_data['head'] >= max_flows(pump_data['flow']/1e3, pump_type=pump_type)*1e2]
+    # Filter out ????
+
+    # Filter out data points with speed less than 50%
+    # pump_data = pump_data[pump_data['speed'] >= 50]
+
+    return pump_data
 
 
 if __name__ == "__main__":
-    test_pump_param_sweep()
+    #
+    test_pairs = create_test_pairs(flow_ub = 3750,flow_lb = 1000,head_ub = 315,head_lb = 140,num_points = 10)
+    print("Test Pairs:")
+    print(test_pairs)
+    filtered_test_pairs = filter_pump_test_points(test_pairs,pump_type='RO_feed')
+    print("Filtered test pairs:")
+    print(filtered_test_pairs)
+    dataset = test_pump_param_sweep(test_pairs=filtered_test_pairs,pump_type='RO_feed',Pin=14.5)
+    print("Dataset:")
+    print(dataset)
+    dataset.to_csv("temp_pump_surr_data.csv", index=False)
